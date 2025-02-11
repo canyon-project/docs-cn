63fa1127742116c682692724c70fff454e450b05
wr.zhang25 <wr.zhang25@gmail.com>
2025-02-11 22:35:38 +0800

    最小可用版本原则，先删除ut文档

--- app/documentation/_meta.js
+++ app/documentation/_meta.js
@@ -1,8 +1,7 @@
 export default {
   "getting-started": "开始",
-  "installation": "安装",
+  installation: "安装",
   "core-concepts": "核心概念",
   "end-to-end-testing": "E2E 测试",
-  "unit-testing": "单元测试",
   ecosystem: "生态",
 };
